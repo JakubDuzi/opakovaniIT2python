766802101661092861a1cc3d5c18b6d2d29b9a79
JakubDuzi <159249308+JakubDuzi@users.noreply.github.com>
2024-09-15 15:01:59 +0000

    Dodělání popisků

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import random
 
-# 1.
+# 1. Proměnná jména
 jmeno = "Jan"
 
-# 2.
+# 2. Proměnná příjmení
 prijmeni = "Novak"
 
-# 3.
+# 3. Vypsání proměnných
 print("Jméno: " + jmeno)
 print("Příjmení: " + prijmeni)
 
-# 4. 9. a bonus. Dal jsem je k sobě
+# 4. 9. a bonus. Dal jsem je k sobě 
+# isdigit kontroluje zda to jsou jenom čísla a vrací True
+# Pak už to vlastně funguje jako klasický while cyklus
 while True:
     vek_input = input("Zadejte svůj věk: ")
     if vek_input.isdigit():
@@ -20,21 +22,21 @@ while True:
     else:
         print("Zadej jen hceločíselnou hodnotu.")
 
-# 5.
+# 5. Vypíše délky jména a příjmění
 print ("Délka jména: " + str(len(jmeno)))
 print ("Délka příjmení: " + str(len(prijmeni)))
 
-# 6.
+# 6. Proměnná s hodnotou 6
 hodnota = 6
 
-# 7.
+# 7. for cyklus s přičítáním čísla 3 pět krát
 for i in range(5):
     hodnota += 3
 
-# 8.
+# 8. Vypíše výslednou hodnotu a str konvertuje hodnotu int na string, jinak to nevypíše
 print("Výsledná hodnota po 5 cyklech: " + str(hodnota))
 
-# 11. 
+# 11. Zde se využáje imporované funkce random a to tím že vygenerujeme pomocí ní náhodné číslo a následně printem vypíšeme
 nahodna_hodnota = random.randint(1, 10)
 print("Náhodná hodnota od 1 do 10: " + str(nahodna_hodnota))
 
